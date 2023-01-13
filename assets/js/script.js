6dbee70b3909295bc2af488b9f0974d67c597eca
Molly Adamsson <adamssonmolly@gmail.com>
2023-01-13 12:09:35 +0000

    Add functions so that the game stops after 5 rounds, player has to press reset game to play again

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,23 @@ function resetGame() {
   document.getElementById("result").innerHTML ="";
 }
 
+let maxRounds = 6;
+
+function disableButtons() {
+  document.getElementById("rock").disabled = true;
+  document.getElementById("paper").disabled = true;
+  document.getElementById("scissors").disabled = true;
+  document.getElementById("lizard").disabled = true;
+  document.getElementById("spock").disabled = true;
+}
+
+function checkRoundLimit() {
+  if (round === maxRounds) {
+    disableButtons();
+    document.getElementById("result").innerHTML = "Game over! Press the reset button to play again.";
+  }
+}
+
 let timer;
 
 function startTimer(){
@@ -74,6 +91,28 @@ document.getElementById("time-button").addEventListener("click", function(){
   }
 });
 
+document.getElementById("reset-button").addEventListener("click", resetGame);
+
+function resetGame() {
+  computerScore = 0;
+  playerScore = 0;
+  round = 1;
+  document.getElementById("round").innerHTML = "Round: " + round;
+  document.getElementById("player-score").innerHTML = "Player Score: " + playerScore;
+  document.getElementById("computer-score").innerHTML = "Computer Score: " + computerScore;
+
+  clearInterval(timer);
+  document.getElementById("time-button").innerHTML = "Start Game";
+  isTiming = false;
+  document.getElementById("result").innerHTML ="";
+ 
+  document.getElementById("rock").disabled = false;
+  document.getElementById("paper").disabled = false;
+  document.getElementById("scissors").disabled = false;
+  document.getElementById("lizard").disabled = false;
+  document.getElementById("spock").disabled = false;
+}
+
 let isMuted = false;
 
 document.getElementById("mute-button").addEventListener("click", toggleMute);
@@ -87,23 +126,24 @@ document.getElementById("mute-button").addEventListener("click", toggleMute);
     document.getElementById("mute-button").innerHTML = isMuted ? "Unmute" : "Mute";
   }
 
-function play(playerChoice) {
-  let computerChoice = getComputerChoice();
-  let result = getResult(playerChoice, computerChoice);
-
-  document.getElementById("round").innerHTML = "Round: " + round;
-  document.getElementById("player-score").innerHTML = "Player Score: " + playerScore;
-  document.getElementById("computer-score").innerHTML = "Computer Score: " + computerScore;
-
-  document.getElementById("result").innerHTML = "You chose " + playerChoice + ", the computer chose " + computerChoice + ". " + result;
-  round++;
+  function play(playerChoice) {
+    let computerChoice = getComputerChoice();
+    let result = getResult(playerChoice, computerChoice);
+  
+    document.getElementById("round").innerHTML = "Round: " + round;
+    document.getElementById("player-score").innerHTML = "Player Score: " + playerScore;
+    document.getElementById("computer-score").innerHTML = "Computer Score: " + computerScore;
+  
+    document.getElementById("result").innerHTML = "You chose " + playerChoice + ", the computer chose " + computerChoice + ". " + result;
+    round++;
+    checkRoundLimit();
   if(round>5){
       if(computerScore>playerScore){
-          document.getElementById("result").innerHTML += "<br> You were smart, but the computer wins the game!";
+          document.getElementById("result").innerHTML += "<br> <br> You were smart, but the computer wins the game!";
       } else if (computerScore<playerScore) {
-          document.getElementById("result").innerHTML += "<br> Well done, you won the game! :D";
+          document.getElementById("result").innerHTML += "<br> <br> Well done, you won the game! :D";
       } else {
-          document.getElementById("result").innerHTML += "<br> But sadly, It's a tie!";
+          document.getElementById("result").innerHTML += "<br> <br> But the result is: it's a tie!";
       }
   }
 }
